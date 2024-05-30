8578c712539c35b4e9c2939fa871799c897b14a3
Az1n <camilo_jrl@hotmail.com>
2024-05-29 19:43:53 -0500

    validations and responses

--- back/src/users/dto/users.dto.ts
+++ back/src/users/dto/users.dto.ts
@@ -1,6 +1,21 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class UserDto {
+
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+
+    @IsString()
+    @IsNotEmpty()
     userName: string;
+
+    @IsString()
+    @IsNotEmpty()
     password: string;
+    
 }
